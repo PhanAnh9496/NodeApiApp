3b4ca21c2b46024866d434f9830cc3b3791e59e9
PhanAnh9496 <PhanAnh9496@gmail.com>
2019-05-09 16:19:51 +0700

    Update_2 09/05/19

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const formidable = require('formidable');
+const fs = require('fs');
 const _ = require('lodash');
 const User = require('../models/user');
 
@@ -43,22 +45,60 @@ exports.getUser = (req, res) => {
     return res.json(req.profile);
 };
 
+// exports.updateUser = (req, res, next) => {
+//     let user = req.profile;
+//     user = _.extend(user, req.body);
+//     user.updated = Date.now();
+//     user.save((err) => {
+//         if (err) {
+//             return res.status(400).json({
+//                 error: "Bạn không được phép thực hiện hành động này"
+//             });
+//         }
+//         user.hashed_password = undefined;
+//         user.salt = undefined;
+//         res.json({user});
+//     });
+// };
+
 exports.updateUser = (req, res, next) => {
-    let user = req.profile;
-    user = _.extend(user, req.body);
-    user.updated = Date.now();
-    user.save((err) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
         if (err) {
             return res.status(400).json({
-                error: "Bạn không được phép thực hiện hành động này"
+                error: "Ảnh không thể tải lên."
             });
         }
-        user.hashed_password = undefined;
-        user.salt = undefined;
-        res.json({user});
+        let user = req.profile;
+        user = _.extend(user, fields);
+        user.updated = Date.now();
+        if (files.photo) {
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+        }
+
+        user.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            res.json(user);
+        });
+
     });
 };
 
+exports.userPhoto = (req, res, next) => {
+    if (req.profile.photo.data) {
+        res.set(("Content-Type", req.profile.photo.contentType));
+        return res.send(req.profile.photo.data);
+    }
+    next();
+};
 
 exports.deleteUser = (req, res, next) => {
     let user = req.profile;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {userById, allUsers, getUser, updateUser, deleteUser} = require('../controllers/user');
+const {userById, allUsers, getUser, updateUser, deleteUser, userPhoto} = require('../controllers/user');
 const {requireSignin} = require('../controllers/auth');
 const router = express.Router();
 
@@ -7,6 +7,9 @@ router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
 router.put('/user/:userId', requireSignin, updateUser);
 router.delete("/user/:userId", requireSignin, deleteUser);
+
+// photo
+router.get("/user/photo/:userId", userPhoto);
 
 router.param('userId', userById);
 
